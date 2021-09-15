00741740cfb545b6de8c8090cadffe29e4f7b57a
Sriharsha Tikkireddy <sri.tikkireddy@databricks.com>
2021-09-15 19:21:06 -0400

    updated default view to autoloader

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import AutoLoader from "./autoloader";
 
-export default () => (
-  <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static</h1>
-  </div>
-)
+// export default () => (
+//   <div style={{ textAlign: 'center' }}>
+//     <h1>Welcome to React-Static</h1>
+//   </div>
+// )
+
+export default AutoLoader;
